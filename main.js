da967884228ba1fd26eb025b70b2eb0bbbe69774
kimberly <92147193+pgm-kim@users.noreply.github.com>
2022-06-22 11:30:19 +0200

    Update main.js

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ var x = setInterval(function() {
   var distance = countDownDate - now;
 
   // Time calculations for days, hours, minutes and seconds
+  
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
